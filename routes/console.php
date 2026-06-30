b4b165cf799fe8d6bf2eceec93cb555fecc19f20
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2026-06-30 00:12:54 -0500

    feat: add greet command to console for personalized greetings

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('greet {name}', function ($name) {
+    $this->comment("Hello, {$name}!");
+})->purpose('Greet a user by name');
